e671b5c3737c6584ba0a13daee8a6f2ca6da56a6
John Smith <send2jsmith@gmail.com>
2011-12-22 00:03:42 -0800

    Allow favicons to be fetched in a task executed off the javafx application thread.

--- src/main/java/org/jewelsea/willow/FavIconHandler.java
+++ src/main/java/org/jewelsea/willow/FavIconHandler.java
@@ -1,22 +1,54 @@
 package org.jewelsea.willow;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.concurrent.Task;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import net.sf.image4j.codec.ico.ICODecoder;
 
 import java.awt.image.BufferedImage;
 import java.net.URL;
-import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.*;
 
 /* Helper class for handling favicons for sites */
 public class FavIconHandler {
   /** @return singleton instance */
-  public static FavIconHandler getInstance() { if (instance == null) instance = new FavIconHandler(); return instance; }
-  private static FavIconHandler instance;
+  public  static FavIconHandler  getInstance() { if (instance == null) instance = new FavIconHandler(); return instance; }
+  private static FavIconHandler  instance;
+
+  // a threadpool for fetching favicons.
+  private final  ExecutorService threadpool;
+
+  // max number of threads we will use to simultaneously fetch favicons.
+  private static final int N_FETCH_THREADS = 4;
+
+  // prefix used for the favicon fetching thread.
+  public static final String FAVICON_FETCH_THREAD_PREFIX = "favicon-fetcher-";
+
+  public FavIconHandler() {
+    // initialize the favicon threadpool to the specified number of threads.
+    // the name of the threads are customized so they are easy to recognize.
+    // the status of the threads are set to daemon, so that the application can
+    // exit even if a favicon fetch is in progress or stalled.
+    threadpool = Executors.newFixedThreadPool(N_FETCH_THREADS, new ThreadFactory() {
+      ThreadFactory defaultFactory = Executors.defaultThreadFactory(); 
+      @Override public Thread newThread(Runnable r) {
+        Thread newThread = defaultFactory.newThread(r);
+        newThread.setName(FAVICON_FETCH_THREAD_PREFIX + newThread.getName());
+        newThread.setDaemon(true);
+        return newThread;
+      }
+    });
+  }
 
   /** least recently used cache of favicons */
-  private HashMap<String, ImageView> faviconCache = new LruCache<String, ImageView>(50);
+  private Map<String, ImageView> faviconCache = 
+    new ConcurrentHashMap<String, ImageView>(
+      new LruCache<String, ImageView>(200)
+    );
 
   /**
    * Fetch a favicon for a given location.
@@ -24,44 +56,69 @@ public class FavIconHandler {
    * @return the favicon for the browser location or null if no such favicon could be determined.
    */
   public ImageView fetchFavIcon(final String browserLoc) {
-    String    faviconLoc = null;
-    ImageView favicon    = null;
-
-    try {
-      final int protocolSepLoc = browserLoc.indexOf("://");
-      if (protocolSepLoc > 0) {
-        // workout the location of the favicon.
-        final int pathSepLoc = browserLoc.indexOf("/", protocolSepLoc + 3);
-        String rootLoc = (pathSepLoc > 0) ? browserLoc.substring(0, pathSepLoc) : browserLoc;
-        faviconLoc = rootLoc + "/favicon.ico";
-
-        // fetch the favicon from cache if it is there.
-        favicon = faviconCache.get(faviconLoc);
-        if (favicon != null) return favicon;
-
+    // fetch the favicon from cache if it is there.
+    final String serverRoot = findRootLoc(browserLoc);
+    ImageView cachedFavicon = faviconCache.get(serverRoot);
+    if (cachedFavicon != null) return cachedFavicon;
+    
+    // ok, it wasn't in the cache, create a placeholder, to be used if the site doesn't have a favicon.
+    final ImageView favicon = new ImageView();
+    favicon.setFitHeight(16);
+    favicon.setPreserveRatio(true);
+    
+    // if the serverRoot of the location cannot be determined, just return the placeholder.
+    if (serverRoot == null) return favicon;
+    
+    // store the new favicon placeholder in the cache.
+    faviconCache.put(serverRoot, favicon);
+    
+    // lazily fetch the real favicon.
+    final Task<Image> task = new Task<Image>() {
+      @Override protected Image call() throws Exception {
         // fetch the favicon from the server if we can.
-        URL url = new URL(faviconLoc);
+        URL url = new URL(serverRoot + "/favicon.ico");
+
+        // decode the favicon into an awt image.
         List<BufferedImage> imgs = ICODecoder.read(url.openStream());
+
+        // if the decoding was successful convert to a JavaFX image and return it.
         if (imgs.size() > 0) {
-          final Image fxImage = Util.bufferedImageToFXImage(imgs.get(0));
-          favicon = new ImageView(fxImage);
-          favicon.setFitHeight(16);
-          favicon.setPreserveRatio(true);
+          return Util.bufferedImageToFXImage(imgs.get(0));
+        } else {
+          return null;
         }
       }
-    } catch (Exception e) {
-      // no action required.
-    }
+    };
 
-    // if we determined a location where the favicon should be, save that in the cache,
-    // the favicon will be null if we could not fetch the favicon from where we thought it might be.
-    if (faviconLoc != null) {
-      faviconCache.put(faviconLoc, favicon);
-    }
+    // replace the placeholder in a favicon whenever the lazy fetch completes.
+    task.valueProperty().addListener(new ChangeListener<Image>() {
+      @Override public void changed(ObservableValue<? extends Image> observableValue, Image oldImage, Image newImage) {
+        if (newImage != null) {
+          favicon.setImage(newImage);
+        }  
+      }
+    });
+
+    threadpool.execute(task);
 
     return favicon;
   }
-  
+
+  /**
+   * Determines the root location for the server or null if the browser location does not represent a server
+   * For example http://www.yahoo.com/games => http://www.yahoo.com
+   */    
+  private String findRootLoc(String browserLoc) {
+    final int protocolSepLoc = browserLoc.indexOf("://");
+    if (protocolSepLoc > 0) {
+      // workout the location of the favicon.
+      final int pathSepLoc = browserLoc.indexOf("/", protocolSepLoc + 3);
+      return (pathSepLoc > 0) ? browserLoc.substring(0, pathSepLoc) : browserLoc;
+    }
+    
+    return null;
+  }
+
 }
 
 // todo double check how pathing logic works for different string types.
