ba10428cf65ebb27091f547734b9458d32a55b27
lilyshard <lilyshard@Johns-MacBook-Air.local>
2013-10-31 22:53:26 -0700

    Make LruCache public.

--- src/main/java/org/jewelsea/willow/LruCache.java
+++ src/main/java/org/jewelsea/willow/LruCache.java
@@ -3,7 +3,7 @@ package org.jewelsea.willow;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
-class LruCache<A, B> extends LinkedHashMap<A, B> {
+public class LruCache<A, B> extends LinkedHashMap<A, B> {
   private final int maxEntries;
   public LruCache(final int maxEntries) {
     super(maxEntries + 1, 1.0f, true);
