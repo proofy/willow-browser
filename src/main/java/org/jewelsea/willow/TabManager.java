78dd1f4a9bf94bd4c78e25a15a5cd190e9634343
lilyshard <lilyshard@Johns-MacBook-Air.local>
2013-10-31 22:55:30 -0700

    Place browser tab in a separate file.

--- src/main/java/org/jewelsea/willow/TabManager.java
+++ src/main/java/org/jewelsea/willow/TabManager.java
@@ -21,7 +21,7 @@ import javafx.util.Callback;
 /** Manages a set of active browser windows */
 public class TabManager {
 
-  final private double TAB_PANE_WIDTH = 400;
+  public static final double TAB_PANE_WIDTH = 400;
 
   /** representation of the current browser. */
   final private ReadOnlyObjectWrapper<BrowserWindow> browser = new ReadOnlyObjectWrapper<>();
@@ -68,7 +68,7 @@ public class TabManager {
     );
 
     // add the initialTab to the tabset.
-    addTab(new BrowserTab());
+    addTab(new BrowserTab(this));
 
     // create a button for opening a new tab.
     newTabButton.setTooltip(new Tooltip("Tabulate"));
@@ -80,49 +80,17 @@ public class TabManager {
     tabGraphic.setFitHeight(14);
     newTabButton.setGraphic(tabGraphic);
     newTabButton.onActionProperty().set(actionEvent -> {
-      final BrowserTab newTab = new BrowserTab();
+      final BrowserTab newTab = new BrowserTab(this);
       newTab.setText("New Tab");
       addTab(newTab);
     });
   }
 
-  private void addTab(BrowserTab tab) {
+  void addTab(BrowserTab tab) {
     tabPane.getTabs().add(tab);
     tabPane.getSelectionModel().selectLast();
     if (chromeLocField != null) {
       chromeLocField.requestFocus();
-    }
-  }
-
-  /** Tab associated with a browser window */
-  class BrowserTab extends Tab {
-    /** @return the browser window associated with this tab */
-    public BrowserWindow getBrowser() { return browser; }
-    private BrowserWindow browser = new BrowserWindow();
-    public BrowserTab() {
-      super(""); // todo if we don't have some value set in here, we can never set the tab value (jira?)
-
-      // set the new browser to open any pop-up windows in a new tab.
-      browser.getView().getEngine().setCreatePopupHandler(popupFeatures -> {
-        final BrowserTab browserTab = new BrowserTab();
-        addTab(browserTab);
-        return browserTab.browser.getView().getEngine();
-      });
-      
-      // put some dummy invisible content in the tab otherwise it doesn't show because it has no dimensions.  // todo file jira?
-      Pane spacer = new StackPane();
-      spacer.setMinWidth(TAB_PANE_WIDTH + 35);
-      spacer.setMaxWidth(TAB_PANE_WIDTH + 35);
-      setContent(spacer);
-      
-      // add the tab 
-      graphicProperty().bind(getBrowser().faviconProperty());
-      getBrowser().getView().getEngine().titleProperty().addListener((observableValue, oldValue, newTitle) -> {
-       // todo we already have a listener for the title, might want to repurpose it...
-        if (newTitle != null && !"".equals(newTitle)) {  // todo I wonder if the title would be reset correctly if the page has no title.
-          setText(newTitle);
-        }
-      });
     }
   }
 
